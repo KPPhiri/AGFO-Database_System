270e693eeb6b56f889ac459eeb45be9754a0daac
apexhydra <zachwaterssteam@yahoo.com>
2018-04-21 15:50:03 -0400

    I Finished the functionality of OwnerOverview

---
+++ src/ConfirmedProperties.java
@@ -0,0 +1,39 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+public class ConfirmedProperties {
+    @FXML
+    public TableColumn namecol;
+    @FXML
+    public TableColumn addrecol;
+    @FXML
+    public TableColumn citycol;
+    @FXML
+    public TableColumn zipcol;
+    @FXML
+    public TableColumn sizecol;
+    @FXML
+    public TableColumn typecol;
+    @FXML
+    public TableColumn pubcol;
+    @FXML
+    public TableColumn commcol;
+    @FXML
+    public TableColumn idcol;
+    @FXML
+    public TableColumn vericol;
+    @FXML
+    public TableColumn ratcol;
+    @FXML
+    private Button searchbut;
+    @FXML
+    private Button managebut;
+    @FXML
+    private Button backbut;
+    @FXML
+    private SplitMenuButton searchspin;
+    @FXML
+    private TextField searchbar;
+}
---
+++ src/VisitorOverview.java
@@ -0,0 +1,26 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+public class VisitorOverview {
+    @FXML
+    public TableColumn usernamecol;
+    @FXML
+    public TableColumn emailcol;
+    @FXML
+    public TableColumn logcol;
+    @FXML
+    private Button deleteaccbut;
+    @FXML
+    private Button deletelogbut;
+    @FXML
+    private Button backbut;
+    @FXML
+    private SplitMenuButton searchspin;
+    @FXML
+    private TextField searchbar;
+    @FXML
+    private Button searchbut;
+
+}
