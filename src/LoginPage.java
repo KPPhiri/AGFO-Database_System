83169ec50f04f9713e0c809dc5a7bd7846ccedcf
TheKeeperOfCurry <35737432+TheKeeperOfCurry@users.noreply.github.com>
2018-04-21 16:48:48 -0400

    indent issue

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -54,16 +54,16 @@ public class LoginPage implements Initializable{
                 // TODO: you need to replace the Select SQL statement's password variable in the WHERE portion to pass variable.
                 // TODO: This will be done when you can actually sign in.
                     email = email_TextField.getText().toString();
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                byte[] bytesOfPass = password_PasswordField.getText().getBytes("UTF-8");
-                byte[] digest = md.digest(bytesOfPass);
-                String pass;
-                StringBuffer stringBuffer = new StringBuffer();
-                for (int i = 0; i < digest.length; i++) {
-                    stringBuffer.append(Integer.toString((digest[i] & 0xff) + 0x100, 16)
-                            .substring(1));
-                }
-                pass = stringBuffer.toString();
+                    MessageDigest md = MessageDigest.getInstance("MD5");
+                    byte[] bytesOfPass = password_PasswordField.getText().getBytes("UTF-8");
+                    byte[] digest = md.digest(bytesOfPass);
+                    String pass;
+                    StringBuffer stringBuffer = new StringBuffer();
+                    for (int i = 0; i < digest.length; i++) {
+                        stringBuffer.append(Integer.toString((digest[i] & 0xff) + 0x100, 16)
+                                .substring(1));
+                    }
+                    pass = stringBuffer.toString();
 
                 ResultSet val = server.createStatement().executeQuery("SELECT Username, U_type, Email FROM USER WHERE Email = '" + email +  "' AND Password = '" + password + "'");
 
